068497e9f031535d12116d9905c87fca63877d71
Leo <277007013+LeoR156@users.noreply.github.com>
2026-07-09 09:52:11 +0500

    Fix CI

--- test_bot.py
+++ test_bot.py
@@ -3,12 +3,12 @@ from weather_utils import get_weather_comment
 
 def test_weather_comment_hot():
     # Проверяем, что при +30 будет "Жара! ☀️"
-    assert get_weather_comment(30) == "Жара!"
+    assert get_weather_comment(30) == "Жара! ☀️"
 
 def test_weather_comment_cold():
     # Проверяем, что при -10 будет "Дубак! ❄️"
-    assert get_weather_comment(-10) == "Дубак"
+    assert get_weather_comment(-10) == "Дубак! ❄️"
 
 def test_weather_comment_normal():
     # Проверяем промежуточное значение
-    assert get_weather_comment(15) == "Норма"
+    assert get_weather_comment(15) == "Нормальная погода ☁️"
--- weather_utils.py
+++ weather_utils.py
@@ -1,10 +1,10 @@
+# weather_utils.py
+
 def get_weather_comment(temperature):
-
+    """Возвращает текстовый комментарий и эмодзи в зависимости от температуры"""
     if temperature >= 25:
-	return "Жара!"
-
+        return "Жара! ☀️"
     elif temperature <= 0:
-	return "Дубак"
-
+        return "Дубак! ❄️"
     else:
-	return "Норма"
+        return "Нормальная погода ☁️"
